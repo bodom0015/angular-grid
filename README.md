26fda21bdc9950dd8d0dcf735de573753c623514
mlambert <mlambert@pavlovmedia.com>
2015-10-18 23:27:59 -0500

    Added keyword

---
+++ README.md
@@ -0,0 +1,100 @@
+## Synopsis
+
+This is a primitive grid service for AngularJS with basic logic for filtering, sorting, paging, and selecting
+
+[Live Demo!](http://bodom0015.game-server.cc/bower_components/angular-grid/demo.html)
+
+## Motivation
+Tables are a common occurence in management dashboard UIs, so I have had to create quite a few of these in the past. Many grids already exist for AngularJS, and I decided to try and make one myself as an exercise.
+
+Currently it needs many bindings to get it working to the point that the demo works, but I am working toward making that a directive soon, which should alleviate much of the custom binding and events.
+
+## Installation
+Bower integration possibly in the future.
+
+Until then, running the following command should retrieve a copy of the source code:
+```git
+git clone https://github.com/bodom0015/angular-grid.git
+```
+
+The source code includes a `demo.html` page which illustrates the usage of this module.
+
+To use the module, add a reference to the javascript to your `index.html`:
+```html
+<script src="angular-grid/angular-grid.js"></script>
+```
+    
+Add the `ngGrid` module to your module's instantiation and pass the Grid service into your Controller:
+```js
+angular
+.module('gridTest', [ 'ngGrid' ])
+.controller('GridController', [ '$scope', 'Grid', function($scope, Grid) {
+  // Creates a new instance of the alert service
+  var arrayOfPageSizes = [ 5, 10, 15, 20 ];
+  $scope.data = [
+    { id: 2, name: 'Alice', number:1234 },
+    { id: 1, name: 'Brian', number:2345 },
+    { id: 4, name: 'Craig', number:3456 },
+    { id: 3, name: 'Dana', number:4567 },
+    { id: 6, name: 'Ellen', number:5678 },
+    { id: 5, name: 'Frank', number:6789 },
+    { id: 8, name: 'Greta', number:7890 },
+    { id: 7, name: 'Henry', number:8901 },
+    { id: 10, name: 'Ixtchel', number:9012 },
+    { id: 9, name: 'Jesus', number:0123 },
+    { id: 12, name: 'Karl', number:0000 },
+    { id: 11, name: 'Laura', number:1111 },
+    { id: 14, name: 'Mike', number:2222 },
+    { id: 13, name: 'Nora', number:3333 },
+    { id: 16, name: 'Othello', number:4444 },
+    { id: 15, name: 'Persephone', number:5555 },
+    { id: 18, name: 'Quartz', number:6666 },
+    { id: 17, name: 'Ryan', number:7777 },
+    { id: 20, name: 'Serpentine', number:8888 },
+    { id: 19, name: 'Toronto', number:9999 },
+    { id: 22, name: 'Uranium', number:1010 },
+    { id: 21, name: 'Volt', number:1212 },
+    { id: 24, name: 'Warhead', number:1313 },
+    { id: 23, name: 'Xylophone', number:1414 },
+    { id: 26, name: 'Yalda', number:1515 },
+    { id: 25, name: 'Zlotnicki', number:1616 },
+  ];
+  
+  // Pass in the pageSizes and a callback to retrieve the scope data
+  $scope.grid = new Grid(arrayOfPageSizes, function() { return $scope.data; });
+}]);
+```
+
+You should then be able to bind to this grid in the view template:
+```html
+<div ng-controller="GridController">
+  <table class="table table-striped table-condensed table-hover" ng-if="grid.data(grid.query).length > 0">
+    <thead>
+      <tr>
+        <td style="width:5%" ng-click="grid.selector.select.all('name')"><input type="checkbox" ng-checked="grid.selector.selection.length === grid.data(grid.query).length"/></td>
+        <th style="width:20%" ng-click="grid.sort.sortBy('id')">
+          ID&nbsp;&nbsp;<i class="fa" ng-show="grid.sort.orderByFieldName === 'id'" ng-class="{ 'fa-sort-numeric-asc': !grid.sort.reverse, 'fa-sort-numeric-desc':  grid.sort.reverse }"></i>
+        </th>
+        <th style="width:50%" ng-click="grid.sort.sortBy('name')">
+          Name&nbsp;&nbsp;<i class="fa" ng-show="grid.sort.orderByFieldName === 'name'" ng-class="{ 'fa-sort-alpha-asc': !grid.sort.reverse, 'fa-sort-alpha-desc': grid.sort.reverse }"></i>
+        </th>
+        <th style="width:25%" ng-click="grid.sort.sortBy('number')">
+          Number&nbsp;&nbsp;<i class="fa" ng-show="grid.sort.orderByFieldName === 'number'" ng-class="{ 'fa-sort-asc': !grid.sort.reverse, 'fa-sort-desc': grid.sort.reverse }"></i>
+        </th>
+      <tr>
+     </thead>
+     <tbody>
+      <tr ng-click="grid.selector.select.toggle(item.name)" ng-repeat="item in grid.data(grid.query) | orderBy:grid.sort.orderByFieldName:grid.sort.reverse | limitTo:grid.pager.pageSize:(grid.pager.pageNum * grid.pager.pageSize)">
+        <td><input type="checkbox" ng-checked="grid.selector.selection.indexOf(item.name) !== -1" /></td>
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.number }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+```
+
+## License
+
+MIT
---
+++ angular-grid.js
@@ -0,0 +1,159 @@
+angular
+.module('ngGrid', [])
+.filter('filterBy', [function () {
+  return function (input, keyName, query, caseInsensitive) {
+    if (!angular.isArray(input) || query === '') {
+      return input;
+    }
+    
+    var ret = [];
+    angular.forEach(input, function(item) {
+      var name = caseInsensitive ? angular.lowercase(item[keyName]) : item[keyName];
+      var q = caseInsensitive ? angular.lowercase(query) : query;
+      if (name && q && name.indexOf(q) !== -1) {
+        ret.push(item);
+      }
+    });
+    
+    return ret;
+  };
+}])
+.service('Grid', [ '$filter', function($filter){
+    return function(pageSize, getData) {
+        var grid = {};
+        
+        grid.query = '';
+        grid.exact = false;
+        
+        // This is a function with a single parameter that should perform filtering for us
+        grid.data = function(query) { 
+          if (grid.query) {
+            return $filter('filterBy')(getData(), 'name', grid.query, !grid.exact);
+          } else {
+            return getData();
+          }
+        };
+        
+        // Define grid selection functions / variables here
+        var selector = {};
+        
+        selector.selection = [];
+        selector.select = {
+            none: function() {
+                selector.selection = [];
+            },
+            single: function(id) {
+                if (selector.selection === id) {
+                    selector.selection = [];
+                } else {
+                    selector.selection = id;
+                }
+                console.log("Selection: " + selector.selection);
+            },
+            toggle: function(id) {
+                var index = selector.selection.indexOf(id);
+                if (index === -1) {
+                    selector.selection.push(id);
+                } else if (index > -1) {
+                    selector.selection.splice(index, 1);
+                }
+                console.log("Selection: " + selector.selection);
+            },
+            all: function(fieldName) {
+                if (selector.selection.length === grid.data(grid.query).length) {
+                    // De-select all
+                    selector.selection = [];
+                    console.log("De-selected all locations");
+                } else {
+                    // Select all
+                    angular.forEach(grid.data(grid.query), function(value) {
+                        if (selector.selection.indexOf(value[fieldName]) === -1) {
+                            selector.selection.push(value[fieldName]);
+                        }
+                    });
+                    console.log("Selected all locations: " + selector.selection);
+                }
+            }
+        };
+        grid.selector = selector;
+        
+        // Define grid sorting functions / variables here
+        var sorter = {};
+        
+        sorter.reverse = false;
+        sorter.orderByFieldName = 'id';
+        sorter.sortBy = function(fieldName) {
+            if (sorter.orderByFieldName === fieldName) {
+                sorter.reverse = !sorter.reverse;
+            } else {
+                sorter.reverse = false;
+                sorter.orderByFieldName = fieldName;
+            }
+        };
+
+        grid.sort = sorter;
+        
+        // Define grid paging functions / variables here
+        var pager = {};
+        
+        pager.pageNum = pager.numberOfPages = 0;        
+        if (angular.isArray(pageSize) && pageSize.length > 0) {
+            pager.pageSizes = pageSize;
+            pager.pageSize = pageSize[0];
+        } else if (angular.isNumber(pageSize)) {
+            pager.pageSizes = [ pageSize ];
+            pager.pageSize = pageSize;
+        }
+        pager.getPageRange = function(pageNum) {
+          var range = [ pageNum-2, pageNum-1, pageNum, pageNum+1, pageNum+2 ];
+          var sanitized = [];
+          angular.forEach(range, function(item) {
+            //if (pager.isValidPage(item)) {
+              sanitized.push(item);
+            //}
+          });
+          return sanitized;
+        };
+        pager.isValidPage = function(page) {
+          return page >= 0 && page < grid.pager.numberOfPages;
+        };
+        pager.isCurrentPage = function(page) {
+          return grid.pager.pageNum === page;
+        };
+        pager.firstPage = function() {
+            pager.pageNum = 0;
+        };
+        pager.disableFirstPage = function() {
+            return pager.disablePrevPage();
+        };
+        pager.prevPage = function() {
+            pager.pageNum = pager.pageNum - 1;
+        };
+        pager.disablePrevPage = function() {
+            return pager.pageNum === 0;
+        };
+        pager.nextPage = function() {
+            pager.pageNum = pager.pageNum + 1;
+        };
+        pager.disableNextPage = function() {
+            return (pager.pageNum + 1) * pager.pageSize >= grid.data(grid.query).length;
+        };
+        pager.lastPage = function() {
+            grid.pager.pageNum = grid.pager.numberOfPages - 1;
+        };
+        pager.disableLastPage = function() {
+            return pager.pageNum === (pager.numberOfPages - 1);
+        };
+        pager.pageSizeChanged = function() {
+            var totalItems = grid.data(grid.query).length;
+            pager.numberOfPages = Math.ceil(totalItems / pager.pageSize) || 1;
+            if (pager.pageNum > pager.numberOfPages) {
+                pager.pageNum = pager.numberOfPages - 1;
+            }
+        };
+
+        grid.pager = pager;
+        
+        return grid;
+    }
+  }]);
